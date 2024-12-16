5bd5466c43c9aa896a90a4c2f16576bd63be2312
JSalord IEDIB <jsalord@iedib.net>
2024-12-16 12:45:05 +0100

    Nova pàgina de benvinguda

---
+++ visor_dades_nasa/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NasaDataComponent } from './nasa-data/nasa-data.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'nasa-data', component: NasaDataComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
